dc647ca3f1ce3c332b14c38bb9fc34e5fca3c6c5
Yusheng Chou <mm7217373@gmail.com>
2026-04-01 15:56:25 +0800

    init index

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/pages/Home.vue')
+  },
+]
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/vue3-playground/',
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia'
+      ],
+      dirs: [
+        'src/composables',
+        'src/stores'
+      ],
+      dts: 'src/auto-imports.d.ts',
+      vueTemplate: true,
+    }),
+    UnoCSS()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 })
